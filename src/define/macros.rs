e61c06661b103a3592e0b5b86f6b901a76d305b8
MaxXSoft <x@MaxXSoft.net>
2021-04-28 17:44:53 +0800

    (macros) added macro 'unwrap_struct'

--- src/define/macros.rs
+++ src/define/macros.rs
@@ -1,11 +1,23 @@
-/// Unwraps an enumerate.
+/// Unwraps an enumeration.
 #[macro_export]
 macro_rules! unwrap_enum {
-  ($v:expr, $t:path) => {
-    if let $t(val) = $v {
+  ($v:expr, $p:path) => {
+    if let $p(val) = $v {
       val
     } else {
       panic!("Failed to unwrap enum!")
+    }
+  };
+}
+
+/// Unwraps a structure.
+#[macro_export]
+macro_rules! unwrap_struct {
+  ($v:expr, $p:path, $($t:tt),*) => {
+    if let $p { $($t,)* } = $v {
+      ($($t,)*)
+    } else {
+      panic!("Failed to unwrap struct!")
     }
   };
 }
