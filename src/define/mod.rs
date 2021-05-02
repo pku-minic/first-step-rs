7f3d9ea1346a6040e07465cf923b625509783ee3
MaxXSoft <x@MaxXSoft.net>
2021-05-02 10:37:55 +0800

    (define) updated module 'define'

--- src/define/mod.rs
+++ src/define/mod.rs
@@ -3,6 +3,6 @@ mod macros;
 mod nested;
 mod token;
 
-pub use ast::{Ast, AstBox};
+pub use ast::{Ast, AstBox, AstVisitor};
 pub use nested::{NestedMap, NestedMapPtr};
 pub use token::{Keyword, Operator, Token};
