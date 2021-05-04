fced640e807795de07f2f1d7c055088dc6e03c0c
MaxXSoft <x@MaxXSoft.net>
2021-05-04 10:05:41 +0800

    (define) updated module 'define'

--- src/define/mod.rs
+++ src/define/mod.rs
@@ -1,8 +1,10 @@
 mod ast;
+mod ir;
 mod macros;
 mod nested;
 mod token;
 
 pub use ast::{Ast, AstBox, AstVisitor};
+pub use ir::{FunDefBox, FunctionDef, Inst, InstBox, ValBox, Value};
 pub use nested::NestedMap;
 pub use token::{Keyword, Operator, Token};
